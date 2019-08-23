446d369d8a3ec166cf28e39cc9bd5465f3d69473
Rachel Grier <rgrier104@gmail.com>
2019-08-23 20:16:50 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,37 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/' do
+    erb :index
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+
+    erb :'/figures/show'
+  end
+
+  post '/figures' do
+
+    @figure = Figure.create(params[:figure])
+
+    if !params[:figure][:landmark_ids].empty?
+      @figure.landmarks << Landmark.find(params[:figure][:landmark_ids])
+    else
+      @new_landmark = Landmark.create(params[:landmark][:name])
+      @figure.landmarks << @new_landmark
+    end
+
+    redirect to "/figures/#{@figure.id}"
+  end
+
 end
